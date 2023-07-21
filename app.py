1aa30ad6e1ca96e028501fcf47efddae7838fc2e
Ranjit singh <jiradhey402@gmail.com>
2023-07-21 23:01:29 +0530

    contact section edited

--- app.py
+++ app.py
@@ -13,6 +13,20 @@ def resume():
 
 
 
+@app.route('/thanks', methods=['GET','POST'])
+def thanks():
+    if request.method=='POST':
+        name = request.form['name']
+        email = request.form['email']
+        subject = request.form['subject']
+        message = request.form['message']
+        detail = [name,email,subject,message]
+        detail = {'name':name,'email':email,'subject':subject,'message':message}
+
+        
+        return render_template('thanks.html' ,detail_list=detail)
+
+
 @app.route('/nlpprojects')
 def nlpprojects():
     return render_template('nlpproject.html')
